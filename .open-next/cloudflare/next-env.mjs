ff6b4239d3a83925c0d1b1adea56138fea5cedc1
muhammad raya ar rizki <muhammadrayaarrizki@gmail.com>
2025-06-03 01:38:58 +0700

    Delete .open-next directory

--- .open-next/cloudflare/next-env.mjs
+++
@@ -1,3 +0,0 @@
-export const production = {"GMAIL_PASSWORD":"keso vvds vmos mndy","GMAIL_USERNAME":"muhammadrayaarrizki@gmail.com"};
-export const development = {"GMAIL_PASSWORD":"keso vvds vmos mndy","GMAIL_USERNAME":"muhammadrayaarrizki@gmail.com"};
-export const test = {"GMAIL_PASSWORD":"keso vvds vmos mndy","GMAIL_USERNAME":"muhammadrayaarrizki@gmail.com"};
